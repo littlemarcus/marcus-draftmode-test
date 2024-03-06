e3c90ce8d8420376f6c932dec02c8a433493d910
Marcus Little <mcolelittle@gmail.com>
2024-03-06 12:38:18 -0500

    add home button

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,14 @@
 import styles from './Footer.module.css'
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
         <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <Link href="/">
+        <p>Go to Homepage</p>
+      </Link>
       </footer>
     </>
   )
